2461458c06b7f4f0a142b0013b58f3f742a5436c
DiegoGarcia1928 <145135836+DiegoGarcia1928@users.noreply.github.com>
2023-09-18 10:45:14 -0500

    Add files via upload

---
+++ Ejercicio 7.py
@@ -0,0 +1,33 @@
+#Los cilcos para la progrmacion son secueniales que permiten la realización o ejecución de funciones para poder abreviar o automatizar codigos, esta el ciclo for que significa
+#ciclo while que significa mientras que 
+
+
+
+print ("Tipo de programa que incluya un ciclo")
+
+print ("---------------Inicio de ciclo---------------")
+
+for i in range (10):
+    print (i**2)
+
+print ("Finalice el ciclo")
+
+#El programa que vamos a realizar tiene como finalidad realizar un ciclo donde le pida al usuario ingresar hasta 3 veces un numero en este caso la edad y
+#cuando genere la variable correcta ejecute en pantalla la siguiente accion, por ejemplo: digita tu numero de cedula, 3. Digite si desea saber el numero de 
+#silla donde fue asignado
+
+
+print ("Admision para entrar")
+
+#
+
+Nombre = str (input("Introduzca su nombre: "))
+Edad = int(input("Introduzca su edad: "))
+Identificiación = int(input("Introduzca su número de indentificación: "))
+Ubicación = ()
+
+listEdades = ["15", "16", "17", "18", "19"]
+
+if Edad in listEdades == ["15", "16", "17"]:
+    print("No puede ingresar")
+
